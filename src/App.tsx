4465ba9bdbec4caefb46c20082f6dd8a9d1e1c45
alvarocartalenta <acardozo@talenta.cloud>
2024-12-29 20:21:13 -0500

    feat: add loader

--- src/App.tsx
+++ src/App.tsx
@@ -2,13 +2,14 @@ import { Route, Switch } from "wouter"
 import { Suspense } from "react"
 import { ROUTES } from "./router/routes"
 import { AuthProvider } from "./context/auth";
+import { DotsLoader } from "./modules/common/loader/dotsLoader";
 
 const { Home, Signin, Signup } = ROUTES;
 
 function App() {  return (
     <div className="h-screen bg-slate-100">
       <AuthProvider>
-        <Suspense fallback={<div>Cargando...</div>}>
+        <Suspense fallback={<DotsLoader />}>
           <Switch>
             <Route path='/' component={Home} />
             <Route path='/sign-up' component={Signup} />
---
+++ src/modules/common/loader/dotsLoader/DotsLoader.tsx
@@ -0,0 +1,11 @@
+const DotsLoader = () => (
+  <div className="grid place-content-center h-screen">
+    <div className="flex gap-2">
+      <div className="w-5 h-5 rounded-full animate-pulse bg-gray-200"></div>
+      <div className="w-5 h-5 rounded-full animate-pulse bg-gray-200"></div>
+      <div className="w-5 h-5 rounded-full animate-pulse bg-gray-200"></div>
+    </div>
+  </div>
+);
+
+export default DotsLoader;
